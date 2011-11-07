68efb975dca6986f6162e866c73b9fc9becac3db
Jan Gehring <krimdomu@googlemail.com>
2011-11-07 14:30:41 +0100

    version bump

--- lib/Rex/Apache/Inject.pm
+++ lib/Rex/Apache/Inject.pm
@@ -151,7 +151,7 @@ This will search the file I<application.yml> inside of myapp.tar.gz and replace 
 
 package Rex::Apache::Inject;
 
-our $VERSION = "0.8.99.0";
+our $VERSION = "0.8.99.1";
 
 use strict;
 use warnings;
